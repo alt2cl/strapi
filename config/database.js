c57ed42db2cabca4fe45c56ec615f23e1a680272
alt2cl <aldo@alt2.cl>
2023-07-22 01:20:32 -0400

    se modifica entornno de desarrollo

--- config/database.js
+++ config/database.js
@@ -2,14 +2,17 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("PGHOST", "127.0.0.1"),
-      port: env.int("PGPORT", 5432),
-      database: env("PGDATABASE", "strapi"),
-      user: env("PGUSER", "strapi"),
-      password: env("PGPASSWORD", "password"),
-      ssl: env.bool(true),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      //schema: env("public"),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-    pool: { min: 0 },
+    debug: false,
   },
 });
 
