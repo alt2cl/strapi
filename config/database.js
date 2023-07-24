fb4f080aadf9bd83f36906018f6c5c4dfebdbd83
alt2cl <aldo@alt2.cl>
2023-07-24 12:16:22 -0400

    ajustes de configuracion a bd

--- config/database.js
+++ config/database.js
@@ -1,22 +1,22 @@
 // database.js
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "railway"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "postgres"),
-        ssl: env.bool("DATABASE_SSL_SELF", false),
-      },
-      options: {
-        debug: env.bool("DEBUG", false),
-      },
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "railway"),
+      username: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "postgres"),
+      schema: env("DATABASE_SCHEMA", "public"),
+      ssl: env.bool("DATABASE_SSL", false),
+    },
+    options: {
+      // Otras configuraciones
+      debug: env.bool("DEBUG", false),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
