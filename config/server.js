86e07abab0d93135f6f3d5d23fd1bd88114d4be9
alt2cl <aldo@alt2.cl>
2023-07-27 16:00:03 -0400

    ajustes config

--- config/server.js
+++ config/server.js
@@ -4,6 +4,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", "http://127.0.0.1:1337"),
   app: {
     keys: env.array("APP_KEYS"),
   },
